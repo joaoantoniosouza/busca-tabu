f21b0d1893e9136e2410c19e2668022dee2232cc
joao <jasouza96@gmail.com>
2020-12-15 14:30:14 -0300

    correção dos parâmetros

--- exe.sh
+++ exe.sh
@@ -3,6 +3,6 @@ inst="1 2 3 4 5"
 
 for i in $inst;
 do
-  #EXEC	  INST				                TEMPO	  N EXE   MAX TROCA   POT TROCA   PERTUBACAO  SEED
-  ./pab   'instancias/i0'"${i}"'.txt'	5		    3       1000        4           100
+  #EXEC	  INST				                TAMANHO LISTA   TEMPO	  N EXE   MAX TROCA   POT TROCA  SEED
+  ./pab   'instancias/i0'"${i}"'.txt'	10000	          30      3       0           0
 done
